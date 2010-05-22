6a47043304afd887316229121d52739586895f36
Kerry Buckley <kerryjbuckley@gmail.com>
2010-05-22 20:18:07 +0100

    Calculate mean from scratch (no lib fns)

--- ch3/part2/ex3/Mean.hs
+++ ch3/part2/ex3/Mean.hs
@@ -1,2 +1,8 @@
-mean xs = (sum xs) / (fromIntegral (length xs))
+mySum []     = 0
+mySum (x:xs) = x + (mySum xs)
+
+myLength []     = 0
+myLength (_:xs) = 1 + (myLength xs)
+
+mean xs = (mySum xs) / (fromIntegral (myLength xs))
 
