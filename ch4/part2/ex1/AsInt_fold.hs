be6b1f1bd196733cd0afa2982eb587c3100f1188
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-13 19:48:24 +0100

    Make parseDigits private

--- ch4/part2/ex1/AsInt_fold.hs
+++ ch4/part2/ex1/AsInt_fold.hs
@@ -1,8 +1,5 @@
 import Data.Char (digitToInt)
 import Data.List (foldl')
 
-parseDigits :: Int -> Char -> Int
-parseDigits acc char = acc * 10 + (digitToInt char)
-
-asInt_fold :: String -> Int
 asInt_fold xs = foldl' parseDigits 0 xs
+              where parseDigits acc char = acc * 10 + (digitToInt char)
