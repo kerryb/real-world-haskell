9eace437066dd6e7b276468f762cd13c2c9f097c
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-14 13:22:07 +0100

    Throw exception on non-integer input

--- ch4/part2/ex1/AsInt_fold.hs
+++ ch4/part2/ex1/AsInt_fold.hs
@@ -1,8 +1,8 @@
-import Data.Char (digitToInt)
-import Data.List (foldl')
+import Data.Char
+import Data.List
 
-asInt_fold ""       = error "Empty string"
-asInt_fold "-"      = error "Empty string"
 asInt_fold ('-':xs) = -(asInt_fold xs)
-asInt_fold xs       = foldl' parseDigits 0 xs
-                      where parseDigits acc char = acc * 10 + (digitToInt char)
+asInt_fold xs       | null xs   = error "Empty string"
+                    | otherwise = foldl' parseDigits 0 xs
+                        where parseDigits acc char | isDigit char = acc * 10 + (digitToInt char)
+                                                   | otherwise = error "Not an integer"
