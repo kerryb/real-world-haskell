0d03737e3c2f1f6854e92768c0b98e57691169bd
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-14 19:25:59 +0100

    Throw exception on too-big numbers

--- ch4/part2/ex1/AsInt_fold.hs
+++ ch4/part2/ex1/AsInt_fold.hs
@@ -4,5 +4,6 @@ import Data.List
 asInt_fold ('-':xs) = -(asInt_fold xs)
 asInt_fold xs       | null xs   = error "Empty string"
                     | otherwise = foldl' parseDigits 0 xs
-                        where parseDigits acc char | isDigit char = acc * 10 + (digitToInt char)
+                        where parseDigits acc char | acc < 0 = error "Too big"
+                                                   | isDigit char = acc * 10 + (digitToInt char)
                                                    | otherwise = error "Not an integer"
