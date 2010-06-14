5f9fe7cf4c921a0146612f13c346972f38ad554d
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-14 13:15:45 +0100

    Throw exception on empty input

--- ch4/part2/ex1/AsInt_fold.hs
+++ ch4/part2/ex1/AsInt_fold.hs
@@ -1,6 +1,8 @@
 import Data.Char (digitToInt)
 import Data.List (foldl')
 
+asInt_fold ""       = error "Empty string"
+asInt_fold "-"      = error "Empty string"
 asInt_fold ('-':xs) = -(asInt_fold xs)
-asInt_fold xs = foldl' parseDigits 0 xs
-              where parseDigits acc char = acc * 10 + (digitToInt char)
+asInt_fold xs       = foldl' parseDigits 0 xs
+                      where parseDigits acc char = acc * 10 + (digitToInt char)
