893c045f9d55bd3d212140529777ea517cf35f5a
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-14 13:07:55 +0100

    Handle negative numbers

--- ch4/part2/ex1/AsInt_fold.hs
+++ ch4/part2/ex1/AsInt_fold.hs
@@ -1,5 +1,6 @@
 import Data.Char (digitToInt)
 import Data.List (foldl')
 
+asInt_fold ('-':xs) = -(asInt_fold xs)
 asInt_fold xs = foldl' parseDigits 0 xs
               where parseDigits acc char = acc * 10 + (digitToInt char)
