94f0a4b957ff8fc05f418565c2e41abacd660d65
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-23 08:54:24 +0100

    Add other accessor functions

--- ch5/SimpleJSON.hs
+++ ch5/SimpleJSON.hs
@@ -6,6 +6,22 @@ data JValue = JString String
             | JArray [JValue]
               deriving (Eq, Ord, Show)
 
-getString :: JValue -> Maybe String
 getString (JString s) = Just s
 getString _           = Nothing
+
+getInt (JNumber n)    = Just (truncate n)
+getInt _              = Nothing
+
+getDouble (JNumber n) = Just n
+getDouble _           = Nothing
+
+getBool (JBool b)     = Just b
+getBool _             = Nothing
+
+getObject (JObject o) = Just o
+getObject _           = Nothing
+
+getArray (JArray a)   = Just a
+getArray _            = Nothing
+
+isNull v              = v == JNull
