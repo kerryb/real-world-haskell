058fba4adf594b2c7e83b2dae4dfba46898eeb00
Kerry Buckley <kerryjbuckley@gmail.com>
2010-06-28 20:38:27 +0100

    Add function definitions

--- ch5/Prettify.hs
+++ ch5/Prettify.hs
@@ -1,25 +1,57 @@
 module Prettify where
 
-data Doc = ToBeDefined
-         deriving (Show)
+data Doc = Empty
+         | Char Char
+         | Text String
+         | Line
+         | Concat Doc Doc
+         | Union Doc Doc
+           deriving (Show,Eq)
 
-text :: String -> Doc
-text str = undefined
-
-double :: Double -> Doc
-double num = undefined
-
-(<>) :: Doc -> Doc -> Doc
-a <> b = undefined
+empty :: Doc
+empty = Empty
 
 char :: Char -> Doc
-char c = undefined
+char c = Char c
+
+text :: String -> Doc
+text "" = Empty
+text s  = Text s
+
+double :: Double -> Doc
+double d = text (show d)
+
+(<>) :: Doc -> Doc -> Doc
+Empty <> y = y
+x <> Empty = x
+x <> y = x `Concat` y
+
+line :: Doc
+line = Line
 
 hcat :: [Doc] -> Doc
-hcat xs = undefined
+hcat = fold (<>)
+
+fold :: (Doc -> Doc -> Doc) -> [Doc] -> Doc
+fold f = foldr f empty
 
 fsep :: [Doc] -> Doc
-fsep xs = undefined
+fsep = fold (</>)
+
+(</>) :: Doc -> Doc -> Doc
+x </> y = x <> softline <> y
+
+softline :: Doc
+softline = group line
+
+group :: Doc -> Doc
+group x = flatten x `Union` x
+
+flatten :: Doc -> Doc
+flatten (x `Concat` y) = flatten x `Concat` flatten y
+flatten Line           = Char ' '
+flatten (x `Union` _)  = flatten x
+flatten other          = other
 
 punctuate :: Doc -> [Doc] -> [Doc]
 punctuate p []     = []
@@ -27,4 +59,13 @@ punctuate p [d]    = [d]
 punctuate p (d:ds) = (d <> p) : punctuate p ds
 
 compact :: Doc -> String
-compact a = undefined
+compact x = transform [x]
+    where transform [] = ""
+          transform (d:ds) =
+              case d of
+                Empty        -> transform ds
+                Char c       -> c : transform ds
+                Text s       -> s ++ transform ds
+                Line         -> '\n' : transform ds
+                a `Concat` b -> transform (a:b:ds)
+                _ `Union` b  -> transform (b:ds)
